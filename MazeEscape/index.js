d8efa75df07772bd7defae79cfe78a8308d0ecff
Gin5567 <130166665+Gin5567@users.noreply.github.com>
2025-10-08 00:28:07 +0800

    feat: random maze mode to MazeEscape (#86)
    * ✨ Add random maze mode to MazeEscape
    
    * fix: syntaxerrors with merge
    
    ---------
    
    Co-authored-by: Seniru Pasan Indira <senirupasan@gmail.com>

--- MazeEscape/index.js
+++ MazeEscape/index.js
@@ -2,11 +2,13 @@ const readline = require('readline');
 const esc = require('ansi-escapes');
 const { maps, symbols } = require('./maps.json');
 const Hero = require('./Hero');
+const { generateRandomMaze } = require('./generateRandomMap');
 
 let round = 0;
-let map = maps[round];
-let hero = new Hero(map);
+let map = null;
+let hero = null;
 let gameStart = false;
+let useRandom = false;
 let moveCount = 0;
 let totalMoves = 0;
 
@@ -14,68 +16,82 @@ readline.emitKeypressEvents(process.stdin);
 process.stdin.setRawMode(true);
 
 process.stdin.on('keypress', (str, key) => {
-    if (key.name == 'z' && key.ctrl) {
+    if (!gameStart) {
+        if (key.name === '1') {
+            useRandom = false;
+            startGame();
+        } else if (key.name === '2') {
+            useRandom = true;
+            startGame();
+        }
+        return;
+    }
+
+    if (key.name === 'q' || (key.name === 'z' && key.ctrl)) {
+        console.log("Thank you for playing!");
         console.log(esc.cursorShow);
         process.exit(0);
-    } else if (!gameStart) {
-        gameStart = true;
-        welcome();
-    } else if (hero.hasWon()) {
-        if (round + 1 < maps.length) {
+    }
+
+    if (hero.hasWon()) {
+        if (!useRandom && round + 1 < maps.length) {
             round++;
             map = maps[round];
             hero.nextMap(map);
-            moveCount = 0;
+        } else if (useRandom) {
+            console.log(clr("🎉 Congratulations! You escaped the maze!", "green"));
+            console.log(esc.cursorShow);
+            process.exit(0);
         } else {
-            console.log(clr("Congratulations! You've completed all the maps", "green"));
-            console.log(clr("Your number of moves is " + totalMoves,"blue"));
+            console.log(clr("🎉 Congratulations! You've completed all the maps", "green"));
+            console.log(clr("Your number of moves is " + totalMoves, "blue"));
+            console.log(esc.cursorShow);
             process.exit(0);
         }
-    } else if (key.name === 'q') {
-        console.log("Thank you for playing!");
-	console.log(esc.cursorShow);
-        process.exit(0);
-    }  else {
-        if (gameStart) {
-            switch (key.name) {
-                case "up":
-                    hero.moveUp();
-                    moveCount++;
-                    totalMoves++;
-                    break;
-                case "down":
-                    hero.moveDown();
-                    moveCount++;
-                    totalMoves++;
-                    break;
-                case "left":
-                    hero.moveLeft();
-                    moveCount++;
-                    totalMoves++;
-                    break;
-                case "right":
-                    hero.moveRight();
-                    moveCount++;
-                    totalMoves++;
-                    break;
-            }
-        }
-    } 
+        draw();
+        return;
+    }
+
+    switch (key.name) {
+        case "up":
+            hero.moveUp(); break;
+        case "down":
+            hero.moveDown(); break;
+        case "left":
+            hero.moveLeft(); break;
+        case "right":
+            hero.moveRight(); break;
+        default:
+            return;
+    }
+
+    moveCount++;
+    totalMoves++;
     draw();
 });
+
+function startGame() {
+    if (useRandom) {
+        map = generateRandomMaze(15, 15);
+    } else {
+        map = maps[round];
+    }
+    hero = new Hero(map);
+    gameStart = true;
+    welcome();
+    draw();
+}
 
 function draw() {
     let res = "    " + (symbols.wall + " ").repeat(map[0].length) + "\n";
     for (let row of map) {
         res += "  " + symbols.wall + " ";
-        for (let col of row) {
-            res += col + " ";
-        }
-        res += symbols.wall + '\n';
+        for (let col of row) res += col + " ";
+        res += symbols.wall + "\n";
     }
     res += "    " + (symbols.wall + " ").repeat(map[0].length) + "\n";
     res = res.replace("♟", clr("♟", "yellow")).replace(/▣/g, clr("▣", "green"));
-    
+
     res += `\n${clr("Moves:", "blue")} ${moveCount}\n`;
     res += `${clr("Total Moves:", "blue")} ${totalMoves}\n`;
 
@@ -87,36 +103,34 @@ function draw() {
 }
 
 function welcome() {
-
     const logo = `
          __  __    __    ____  ____    ____  ___   ___    __    ____  ____
         (  \\/  )  /__\\  (_   )( ___)  ( ___)/ __) / __)  /__\\  (  _ \\( ___)
          )    (  /(__)\\  / /_  )__)    )__) \\__ \\( (__  /(__)\\  )___/ )__)
-        (_/\\/\\_)(__)(__)(____)(____)  (____)(___/ \\___)(__)(__)(__)  (____) !!!
-
+        (_/\\/\\_)(__)(__)(____)(____)  (____)(___/ \\___)(__)(__)(__)  (____)
     `;
     print(`
-
     Welcome to
     ${clr(logo, 'yellow')}
-
     Maze escape game for Salif's CLI game collection.
 
     ${clr('Help', 'green')}
      - This is you: ♟
      - This is your goal: ▣
 
-       Move your character using arrow keys and reach your goal while avoiding walls.
+     Move your character using arrow keys and reach your goal while avoiding walls.
 
-    ${clr('Press any key to continue...', 'cian')}
+    ${clr('Select game mode:', 'blue')}
+     ${clr('1.', 'cian')} Play with existing maps (from maps.json)
+     ${clr('2.', 'cian')} Play with random maze (increasing size)
 
+    ${clr('Press 1 or 2 to start...', 'yellow')}
     `);
 }
 
-/*Salif's coloring function*/
 function clr(text, color) {
     const code = { red: 91, green: 92, blue: 34, cian: 96, yellow: 93 }[color];
-    if (code) return "\x1b[" + code + "m" + text + "\x1b[0m";
+    return code ? `\x1b[${code}m${text}\x1b[0m` : text;
 }
 
 function print(str, hide = true) {
